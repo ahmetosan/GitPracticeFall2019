9e5b6d2e22de9ab3151efef5bb3ab3f2541c6bf3
ahmetosan <ahmetpcf@gmail.com>
2020-03-11 20:21:12 -0400

    blue

--- src/day01/Colors.java
+++ src/day01/Colors.java
@@ -2,6 +2,6 @@ package day01;
 
 public class Colors {
     public static void main(String[] args) {
-            String[] colors = {"red", "blue","yellow"};
+            String[] colors = {"red", "blue","yellow","blue"};
     }
 }
